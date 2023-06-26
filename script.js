7ea7609e0de7bab6415b7da342d6129898109d0f
bacigala25 <126465827+bacigala25@users.noreply.github.com>
2023-06-26 14:45:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,22 @@ const validate = () => {
     
     if (email.length == 0){       
         document.querySelector(".wrong-email").classList.remove("hidden")
+        document.querySelector("#input-email").style.borderColor = "rgba(255, 98, 87)"
+        document.querySelector("#input-email").style.borderWidth = "2px";
+        document.querySelector("#input-email").style.borderStyle = "solid";
+        document.querySelector("#input-email").style.backgroundColor = "rgba(255, 98, 87, 0.15)"
+       
     }
     else if (!email.match(/[a-z]+@[a-z]+\.[a-z]+/)){        
         document.querySelector(".wrong-email").classList.remove("hidden")
+        document.querySelector("#input-email").style.borderColor = "rgba(255, 98, 87)"
+        document.querySelector("#input-email").style.borderWidth = "2px";
+        document.querySelector("#input-email").style.borderStyle = "solid";
+        document.querySelector("#input-email").style.backgroundColor = "rgba(255, 98, 87, 0.15)"
+       
     }
     else {
+        document.querySelector("#input-email").style.backgroundColor = "initial";
         subscribe()
     }
 }
